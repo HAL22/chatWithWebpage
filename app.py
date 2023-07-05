30bf619f901f38f39ddc24f320b35de4aebc3fc3
HAL22 <thethelafaltein@gmail.com>
2023-07-05 21:26:23 +0200

    Creating embeddings

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
 import requests
 import tiktoken
 import pinecone
