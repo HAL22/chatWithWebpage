69813a64df13ce4c9cf9cc6c3f1562754b945032
HAL22 <thethelafaltein@gmail.com>
2023-07-05 21:07:00 +0200

    Creating embeddings

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def get_agent(url):
     ]
 
     llm = ChatOpenAI(
-        openai_api_key="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",
+        openai_api_key=st.secrets['OPENAI_API_KEY'],
         model_name='gpt-3.5-turbo',
         temperature=0.0
     )
@@ -105,3 +105,66 @@ def get_agent(url):
 
     return agent
 
+
+# Text input
+txt_input = st.text_area('Enter url', '', height=80)
+
+agent = get_agent(txt_input)
+st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 HugChat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [HugChat](https://github.com/Soulter/hugging-chat-api)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
+    
+    💡 Note: No API key required!
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
+
+# Generate empty lists for generated and past.
+## generated stores AI generated responses
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+## past stores User's questions
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']  
+
+# Layout of input/response containers
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+# User input
+## Function for taking user provided prompt as input
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+## Applying the user input box
+with input_container:
+    user_input = get_text()
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(prompt):
+    chatbot = agent
+    response = chatbot.chat(prompt)
+    return response    
+
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+        
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
