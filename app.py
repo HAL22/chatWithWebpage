a354eb031c22f2e30efe3485c42201f3a6ab8755
HAL22 <thethelafaltein@gmail.com>
2023-07-05 22:27:37 +0200

    Creating embeddings

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
 from chat import *
 
 # Page title
@@ -11,10 +13,33 @@ input_container = st.container()
 #colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 HugChat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [HugChat](https://github.com/Soulter/hugging-chat-api)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
+    
+    💡 Note: No API key required!
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
+
+# Generate empty lists for generated and past.
+## generated stores AI generated responses
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+## past stores User's questions
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']
+
 # Text input
 def get_url_user():
     txt_input = st.text_area('Enter url', '', height=80)
-    return "https://en.wikipedia.org/wiki/Chelsea_F.C"
+    return txt_input
 with get_agent_container:
     agent = get_agent(get_url_user())
 
@@ -30,7 +55,7 @@ def generate_response(prompt):
 
 ## Conditional display of AI generated responses as a function of user provided prompts
 with response_container:
-    if user_input:
+    if user_input and agent:
         response = generate_response(user_input)
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response)
