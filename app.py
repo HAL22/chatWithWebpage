32c97df28be18c55101fc93453b2bbfdcfb049c1
HAL22 <thethelafaltein@gmail.com>
2023-07-08 19:51:46 +0200

    Changing the streamlit UI

--- app.py
+++ app.py
@@ -1,66 +1,29 @@
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
-from chat import *
+import chat
 
-# Page title
-st.set_page_config(page_title='🦜🔗 Chat With Web Page')
-st.title('🦜🔗 Chat with any webpage')
+def show_messages(text):
+    messages_str = [
+        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
+    ]
+    text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
 
-get_agent_container = st.container()
-input_container = st.container()
-#colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
 
-# Sidebar contents
-with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
-    
-    💡 Note: No API key required!
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
+st.header("STREAMLIT GPT-3 CHATBOT")
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
+web_url = st.text_input("Prompt", value="Enter the url here...")
 
-# Text input
-def get_url_user():
-    txt_input = st.text_area('Enter url', '', height=80)
-    return txt_input
-with get_agent_container:
-    agent = get_agent(get_url_user())
+agent_created = False
 
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()    
+if st.button("Generate agent"):
+    if web_url != "":
+        with st.spinner("Generating response..."):
+            agent = chat.get_agent(web_url)
+            agent_created = True
 
-def generate_response(prompt):
-    reponse = agent(prompt)['output']
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input and agent:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+prompt = st.text_input("Prompt", value="Enter your message here...")
+
+if st.button("Send") and agent_created:
+    with st.spinner("Generating response..."):
+        output = agent(prompt)['output']
+        show_messages(output)
