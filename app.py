5e57165e53e79ec8e3e58eaddc989ee8eb0dbc61
HAL22 <thethelafaltein@gmail.com>
2023-07-08 21:41:48 +0200

    Changing the streamlit UI

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ if 'messages' not in st.session_state:
         {"role": "system", "content": "You are a helpful assistant."}
     ]
 
-def generate_response(prompt):
-    agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
+def generate_response(prompt,agent):
     st.session_state['messages'].append({"role": "user", "content": prompt})
     response = agent(prompt)['output']
     st.session_state['messages'].append({"role": "assistant", "content": response})
@@ -27,12 +26,13 @@ response_container = st.container()
 container = st.container()
 
 with container:
+    agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')     
 
     if submit_button and user_input:
-        output = generate_response(user_input)
+        output = generate_response(user_input,agent)
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
 
