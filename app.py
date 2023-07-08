bddc54ef05297cc5ff09bbfb117b2843ca834f23
HAL22 <thethelafaltein@gmail.com>
2023-07-08 21:33:09 +0200

    Changing the streamlit UI

--- app.py
+++ app.py
@@ -2,27 +2,44 @@ import streamlit as st
 from streamlit_chat import message
 import chat
 
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 if 'messages' not in st.session_state:
     st.session_state['messages'] = [
         {"role": "system", "content": "You are a helpful assistant."}
     ]
 
-def generate_response():
-    prompt = st.session_state.user_input
+def generate_response(prompt):
     agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
     st.session_state['messages'].append({"role": "user", "content": prompt})
     response = agent(prompt)['output']
     st.session_state['messages'].append({"role": "assistant", "content": response})
+    return response
 
 st.title("Chat placeholder")
 
-chat_placeholder = st.empty()
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
 
-if st.session_state['generated']:
-    with chat_placeholder:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')     
 
-with st.container():
-    st.text_input("User Input:", on_change=generate_response, key="user_input")            
+    if submit_button and user_input:
+        output = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))  
+             
+                    
