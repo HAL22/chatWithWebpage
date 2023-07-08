e78afd6bc1c11e95cd4f48c87706ab6e618f5644
HAL22 <thethelafaltein@gmail.com>
2023-07-08 21:45:23 +0200

    Changing the streamlit UI

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ st.title("Chat placeholder")
 response_container = st.container()
 # container for text box
 container = st.container()
+agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
 
 with container:
-    agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')     
