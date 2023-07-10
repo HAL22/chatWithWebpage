3b79db109b8fff4e2c2a468e569477fbbd8721fc
HAL22 <thethelafaltein@gmail.com>
2023-07-10 21:33:31 +0200

    Changing the streamlit UI

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ agent = chat.get_agent("https://en.wikipedia.org/wiki/Chelsea_F.C.")
    
         
 with url_container:
-    user_input = st.text_area("Enter url:", key='inpuUrlt', height=100)
-    submit_button_url = st.form_submit_button(label='Send_URL') 
+    with st.form(key='my_form_url', clear_on_submit=True):
+        user_input = st.text_area("Enter url:", key='inpuUrlt', height=100)
+        submit_button_url = st.form_submit_button(label='Send_URL') 
 
 with container:
     with st.form(key='my_form', clear_on_submit=True):
